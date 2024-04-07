66700f658781bb721077d470b0e92a9c7e4d41f3
MWWeinberg <hello@michaelweinberg.org>
2024-04-07 14:23:57 -0400

    added switch to send

--- send.py
+++ send.py
@@ -1,19 +1,20 @@
 import board
 import busio
 import digitalio
+import time
 #radio
 spi = busio.SPI(board.SCK, MOSI=board.MOSI, MISO=board.MISO)
 cs = digitalio.DigitalInOut(board.RFM69_CS)
 reset = digitalio.DigitalInOut(board.RFM69_RST)
+import adafruit_rfm69
+rfm69 = adafruit_rfm69.RFM69(spi, cs, reset, 915.0)
 #buttons
 from digitalio import DigitalInOut, Direction, Pull
 
-import adafruit_rfm69
-rfm69 = adafruit_rfm69.RFM69(spi, cs, reset, 915.0)
-
-
-#just for testing
-import time
+#switch
+switch = DigitalInOut(board.D10)
+switch.direction = Direction.INPUT
+switch.pull = Pull.UP
 
 #buttons
 button1 = DigitalInOut(board.D12)
@@ -21,31 +22,49 @@ button1.direction = Direction.INPUT
 button1.pull = Pull.UP
 
 button2 = DigitalInOut(board.D11)
-button2.direction = Direction.INPUT 
-button2.pull = Pull.UP 
+button2.direction = Direction.INPUT
+button2.pull = Pull.UP
 
 print("Started!")
 while True:
     #print("Hello, CircuitPython!")
     #rfm69.send('Hello world!')
     #time.sleep(2)
-    if button1.value == True:
-        print("off1")
-    if button1.value == False:
-       print("on1")
-       rfm69.send('B1 hello')
-       #cut this after testing -  just so you can see
-       time.sleep(.5)
-    if button2.value == True:
-        print("off2")
-    if button2.value == False:
-       print("on2")
-       rfm69.send('B2 hello')
-       #cut this after testing -  just so you can see
-       time.sleep(.5)
-    
-    
-    
+
+    if switch.value == True:
+        print("true")
+        if button1.value == True:
+            print("off1")
+        if button1.value == False:
+            print("on1")
+            rfm69.send('B1 hello')
+            #cut this after testing -  just so you can see
+            time.sleep(.5)
+        if button2.value == True:
+            print("off2")
+        if button2.value == False:
+            print("on2")
+            rfm69.send('B2 hello')
+            #cut this after testing -  just so you can see
+            time.sleep(.5)
+
+    elif switch.value == False:
+        print("false")
+        if button1.value == True:
+            print("off3")
+        if button1.value == False:
+            print("on3")
+            rfm69.send('B3 hello')
+            #cut this after testing -  just so you can see
+            time.sleep(.5)
+        if button2.value == True:
+            print("off4")
+        if button2.value == False:
+            print("on4")
+            rfm69.send('B4 hello')
+            #cut this after testing -  just so you can see
+            time.sleep(.5)
+
     #else:
         #print("broken")
     time.sleep(0.01)
