7321c478dc79fa4ab0a03df62eb89c93f7f2912e
MWWeinberg <hello@michaelweinberg.org>
2024-04-07 12:20:21 -0400

    added light response to receive

--- recieve.py
+++ recieve.py
@@ -16,16 +16,28 @@ import neopixel
 
 pixel_pin = board.A1
 num_pixels = 8
-pixels = neopixel.NeoPixel(pixel_pin, num_pixels, brightness=0.3, auto_write=False)
+#use GRBW for the sticks, RGB for the circle
+ORDER = neopixel.GRBW
+pixels = neopixel.NeoPixel(pixel_pin, num_pixels, brightness=0.3, auto_write=False, pixel_order=ORDER)
 
 RED = (255, 0, 0)
 GREEN = (0,255,0)
+BLUE = (0,0,255)
 
+
+pixels.fill(BLUE)
+pixels.show()
 while True:
-    pixels.fill(RED)
-    pixels.show()
-    
+
+
     packet = rfm69.receive()  # Wait for a packet to be received (up to 0.5 seconds)
     if packet is not None:
         packet_text = str(packet, 'ascii')
         print('Received: {0}'.format(packet_text))
+        if packet_text == 'B1 hello':
+            print('we have green lights')
+            pixels.fill(GREEN)
+        if packet_text == 'B2 hello':
+            pixels.fill(RED)
+            print('we have red lights')
+        pixels.show()
